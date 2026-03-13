b431ec0115c498b4ff4d52c30dcf1d96386cd1b0
palatete <yanisprime@hotmail.fr>
2026-03-13 15:26:47 +0100

    Initial commit

---
+++ Biblio/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Biblio/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Biblio
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
