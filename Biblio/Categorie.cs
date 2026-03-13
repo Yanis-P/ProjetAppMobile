f3b9e0faf63d77a59fbb83b7833f4b53a9ff03ab
palatete <yanisprime@hotmail.fr>
2026-03-13 16:52:02 +0100

    ajout classe categorie

---
+++ Biblio/Categorie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio
+{
+    public class Categorie
+    {
+        private int idCat;
+        private string name;
+        private int count;
+        private string description;
+        private string image;
+
+        public Categorie(int idCat, string name, int count, string description, string image)
+        {
+            this.IdCat = idCat;
+            this.Name = name;
+            this.Count = count;
+            this.Description = description;
+            this.Image = image;
+        }
+
+        public int IdCat { get => idCat; set => idCat = value; }
+        public string Name { get => name; set => name = value; }
+        public int Count { get => count; set => count = value; }
+        public string Description { get => description; set => description = value; }
+        public string Image { get => image; set => image = value; }
+    }
+}
