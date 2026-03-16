0272af660c36053a9e7cc9378ba2629daed3fba8
palatete <yanisprime@hotmail.fr>
2026-03-16 17:24:03 +0100

    probleme régler avec Ios et macCatalyst + Ajout Grid et Recup donnée API

--- Biblio/MainPage.xaml.cs
+++ Biblio/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Biblio
 {
     public partial class MainPage : ContentPage
@@ -5,6 +7,29 @@ namespace Biblio
         public MainPage()
         {
             InitializeComponent();
+
+            loadDataFromAPI();
+
         }
+
+        public async void loadDataFromAPI()
+        {
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                var restURL = "http://localhost:5034/Categorie";
+                client.BaseAddress = new Uri(restURL);
+                HttpResponseMessage response = await client.GetAsync(restURL);
+                var content = await response.Content.ReadAsStringAsync();
+                var Items = JsonConvert.DeserializeObject<List<Categorie>>(content);
+                lv.ItemsSource = Items;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
