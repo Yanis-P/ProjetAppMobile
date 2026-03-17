c3ff3a9691d32541cdb947682c46bfbe8b25ff28
palatete <yanisprime@hotmail.fr>
2026-03-17 15:37:20 +0100

    affichage + recup donnée API

--- API_Bilblio/Controllers/CategorieController.cs
+++ API_Bilblio/Controllers/CategorieController.cs
@@ -18,11 +18,11 @@ namespace API_Biblio.Controllers
         {
             List<Categorie> categories = new List<Categorie>();
 
-            categories.Add(new Categorie(1, "Romans", 20, "test", "oui.png"));
-            categories.Add(new Categorie(2, "Sciences", 15, "test1", "oui1.png"));
-            categories.Add(new Categorie(3, "Histoire", 12, "test2", "oui2.png"));
-            categories.Add(new Categorie(4, "Jeunesse", 25, "test3", "oui3.png"));
-            categories.Add(new Categorie(5, "Biographie", 10, "test4", "oui4.png"));
+            categories.Add(new Categorie(1, "Romans", 20, "test", "roman.png"));
+            categories.Add(new Categorie(2, "Sciences", 15, "test1", "science.png"));
+            categories.Add(new Categorie(3, "Histoire", 12, "test2", "paff.png"));
+            categories.Add(new Categorie(4, "Jeunesse", 25, "test3", "paff.png"));
+            categories.Add(new Categorie(5, "Biographie", 10, "test4", "paff.png"));
 
             return categories;
         }
